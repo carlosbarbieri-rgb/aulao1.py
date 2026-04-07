1a9873f8e757838f99d7f6f62e64280d46ff20f0
carlosbarbieri-rgb <carlos.barbieri@univille.br>
2026-04-06 21:56:27 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,34 @@
+nome=input('Qual seu nome? ')
+print(f'Olá {nome}, seja bem vindo(a) ')
+
+Valor=int(input('Qual valor você quer dobrar? '))
+print(f'O dobro de {Valor} é {Valor*2}')
+
+Valor1=int(input('digite um numero:') )
+Valor2=int(input('digite mais um numero:'))
+print(f'{Valor1} mais {Valor2} é igual á {Valor1+Valor2}')
+
+ano=int(input('Em que ano você nasceu? '))
+print(f'Sua idade é {2026-ano} anos')
+
+salario=float(input('Qual o seu salário? '))
+print(f'Com um aumento de 10% seu salário agora é R${salario*1.10:.2f}')
+
+Temperatura=float(input('Qual a temperatura em Celsius? '))
+print(f'{Temperatura}ºc em fahrenheit é {Temperatura*9/5+32}ºF')
+
+nome=input('Qual seu nome Completo? ')
+idade=int(input('Qual sua idade? '))
+cidade=input('Qual cidade você mora? ')
+print(f'Nome: {nome}')
+print(f'idade: {idade} anos')
+print(f'Cidade: {cidade}')
+
+valor1=int(input('Digite um valor: '))
+Valor2=int(input('Digite outro valor: '))
+print(f"A soma de {valor1} e {Valor2} é igual a {valor1+Valor2}, a subtração é igual a {valor1-Valor2}, a multiplicação é igual a {valor1*Valor2} e a divisão é igual a {valor1/Valor2}, a divisão inteira é igual a {valor1//Valor2} e o resto da divisão é igual a {valor1%Valor2}")
+
+valoR=float(input('Digite um valor em reais: '))
+cotação=float(5.10)
+print(f' R${valoR} reais é igual a US${valoR/cotação:.2f}')
+
